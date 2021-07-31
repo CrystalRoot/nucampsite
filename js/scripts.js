571d975e8a415f4e06dddcb798ca446a6c8d6f33
Crystal Root <crystal.l.root@gmail.com>
2021-07-31 11:51:38 -0700

    week 4 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,17 @@ $(function() {
         }
     });
 });    
+
+$(function() {
+    $("#reserveButton").on("click", function() {
+        $("#reserveModal").modal('show');
+    });
+});
+
+$(function() {
+    $("#loginButton").on("click", function() {
+        $("#loginModal").modal('show');
+    });
+});
+
+
